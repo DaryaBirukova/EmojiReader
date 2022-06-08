d9ebef102037e7ce4b6a65c0bbe786c734cfabed
Daria Biriukova <dar.biriukova@gmail.com>
2022-06-08 13:29:28 +0700

    Initial commit

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Дарья Бирюкова on 07.06.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
